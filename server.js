5096ddca4c26011140a6085a037cebc6f0d696f3
rasha <82375859+rasha-taher@users.noreply.github.com>
2023-12-20 10:10:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,24 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const bodyParser=require('body-parser');
+const mongoose= require('mongoose');
 const PORT = 8000;
-
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', (error) => {
-  console.error('Error when connecting to database:', error.message);
-});
-
-db.once('open', () => {
-  console.log('Connected to Database');
-});
-
+app.use(bodyParser.json())
 // start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser:true, useUnifiedTopology: true})
+const db = mongoose.connection;
+db.on('error',(error)=> console.error.bind(error , "Error when connceting to database"))
+db.once('open' , ()=> console.log(" Connected to Database"))
+
 const productRoute= require('./routes/productRoutes')
 const vendorRoute= require('./routes/vendoreRoutes')
 const categoryRoute= require('./routes/categoriesRoutes')
